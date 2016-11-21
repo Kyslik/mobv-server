502e5fb9f9f7f6bdcf5f49de954e4fade3b1839d
Martin Kiesel <martin.kiesel@gmail.com>
2016-11-21 17:49:40 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -3,10 +3,9 @@
 # set up alias for git up
 # git config --local alias.up '!git remote update -p; git merge --ff-only @{u}'
 
+# /home/visi.sk/sub/mobv-server/artisan in case artisan command needed
+
 git up
 composer install
 composer dump-autoload -o
-php /var/www/html/application/artisan down
-php /var/www/html/application/artisan clear-compiled
-php /var/www/html/application/artisan cache:clear
-php /var/www/html/application/artisan up
+
