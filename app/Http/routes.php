d5985ac5f28b56af88c9058f85519a6058fd63b8
Martin Kiesel <martin.kiesel@gmail.com>
2016-11-24 23:37:15 +0100

    Add TODOs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,6 +3,9 @@
 $app->group(['prefix' => 'api/v1'], function (Laravel\Lumen\Application $app) {
     $app->group(['prefix' => 'locations'], function (Laravel\Lumen\Application $app) {
         $app->get('/', 'LocationController@index');
+
+        //TODO: maybe add location middleware also here
+        //TODO: maybe rename location middleware to locationExists or something simillar
         $app->get('/{id}', 'LocationController@show');
 
         $app->group(['prefix' => '/{location_id}/access-points', 'middleware' => 'location'],
