20b61753c16909dc0e2e381399a187837c783912
Martin Kiesel <martin.kiesel@gmail.com>
2016-12-02 14:59:26 +0100

    Add timestamps to LocationsSeeder

--- database/seeds/LocationsSeeder.php
+++ database/seeds/LocationsSeeder.php
@@ -3,6 +3,7 @@ use Illuminate\Database\Seeder;
 
 class LocationsSeeder extends Seeder
 {
+
     /*
      * A -1...8
      * B -1...7
@@ -15,33 +16,36 @@ class LocationsSeeder extends Seeder
     {
         \DB::table('locations')->delete();
 
-        $tall_blocks = ['A', 'C'];
-        $short_blocks = ['B', 'D'];
-        $tiny_blocks = ['T'];
-
+        $tall_blocks = [ 'A', 'C' ];
+        $short_blocks = [ 'B', 'D' ];
+        $tiny_blocks = [ 'T' ];
 
         $data = [];
 
         for ($i = -2; $i <= 7; $i++) {
-            $data[] = ['block' => 'E', 'level' => $i];
+            $data[] = [ 'block' => 'E', 'level' => $i ];
         }
-        
+
         foreach ($tall_blocks as $block) {
             for ($i = -1; $i <= 8; $i++) {
-                $data[] = ['block' => $block, 'level' => $i];
+                $data[] = [ 'block' => $block, 'level' => $i ];
             }
         }
 
         foreach ($short_blocks as $block) {
             for ($i = -1; $i <= 7; $i++) {
-                $data[] = ['block' => $block, 'level' => $i];
+                $data[] = [ 'block' => $block, 'level' => $i ];
             }
         }
 
         foreach ($tiny_blocks as $block) {
             for ($i = -1; $i <= 1; $i++) {
-                $data[] = ['block' => $block, 'level' => $i];
+                $data[] = [ 'block' => $block, 'level' => $i ];
             }
+        }
+
+        foreach ($data as &$row) {
+            $row['created_at'] = $row['updated_at'] = \Carbon\Carbon::now();
         }
 
         App\Location::insert($data);
